34d52e19364a8a1fdc590b55ca2515c6d30f206b
Tim Brown <brown.tim.lee@gmail.com>
2024-09-29 20:45:37 -0400

    Port over some theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const america = localFont({
+  src: "./fonts/GT-America-Regular.woff2",
+  variable: "--font-america",
+  weight: "500",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${america.variable}`}>{children}</body>
     </html>
   );
 }
