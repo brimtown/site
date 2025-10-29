7e33c5f7a50438b27178cf59eea7334eed502fc3
Tim Brown <brown.tim.lee@gmail.com>
2025-10-28 22:36:14 -0400

    More styling, remove mobile/desktop header split

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
 import styles from "./header.module.css";
 
@@ -9,46 +7,18 @@ interface Props {
 }
 
 const Header: React.FC<Props> = ({ color }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <nav className={styles.headerWrapper} style={{ color: color }}>
       <Link href="/" className={styles.nameLink}>
         Tim Brown
       </Link>
-      <div className={styles.menuContainer}>
-        <button
-          className={styles.menuButton}
-          onClick={toggleMenu}
-          aria-expanded={menuOpen}
-          aria-label="Menu"
-        >
-          <span className={styles.menuIcon}>⋮</span>
-        </button>
-
-        <div
-          className={`${styles.menuDropdown} ${menuOpen ? styles.menuOpen : ""}`}
-        >
-          <Link href="/blog" className={styles.menuItem}>
-            Blog
-          </Link>
-          <Link href="/links" className={styles.menuItem}>
-            Links
-          </Link>
-        </div>
-
-        <div className={styles.desktopLinks}>
-          <Link href="/blog" className={styles.desktopLink}>
-            Blog
-          </Link>
-          <Link href="/links" className={styles.desktopLink}>
-            Links
-          </Link>
-        </div>
+      <div className={styles.links}>
+        <Link href="/blog" className={styles.link}>
+          Blog
+        </Link>
+        <Link href="/links" className={styles.link}>
+          Links
+        </Link>
       </div>
     </nav>
   );
